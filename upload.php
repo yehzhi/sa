2679b06d1a2cc66740d6e9dee7c333d61335352d
yo08177 <105029848+yo08177@users.noreply.github.com>
2024-05-11 12:30:12 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,15 +2,13 @@
 session_start();
 $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname = "s";
+$password = "19990817";
+$dbname = "sa";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $title = $_POST["title"];
 $address = $_POST["address"];
-//$target_dir = "uploads/";
-//$_FILES['image']['type'];
 $rent = $_POST["rent"];
 $gender = $_POST["gender"];
 $equip = $_POST["eq"];
@@ -19,17 +17,34 @@ $entrance = $_POST["entrance"];
 $walktime = $_POST["walktime"];
 $introduce = $_POST["introduce"];
 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$filename = basename($_FILES["fileToUpload"]["name"]);
+//$photo_data = file_get_contents($_FILES['fileToUpload']['tmp_name']);
+
+
 if(isset($_POST['eq'])) {
     
     $checkbox_values = implode(",", $_POST['eq']);
 
     
-    $sql = "INSERT INTO information(i_title,i_address,i_rent,i_gender,i_equip,i_roomstyle,i_entrance,i_walktime,i_introduce)  VALUES ('$title','$address','$rent','$gender','$checkbox_values','$roomstyle','$entrance','$walktime','$introduce')";
+    $sql = "INSERT INTO information(i_title,i_address,i_photo,i_rent,i_gender,i_equip,i_roomstyle,i_entrance,i_walktime,i_introduce)  VALUES ('$title','$address','$filename','$rent','$gender','$checkbox_values','$roomstyle','$entrance','$walktime','$introduce')";
     if ($conn->query($sql) !== TRUE) {
-        echo "上架失敗!";
+        ?>
+        <script>
+            alert("上架失敗!");
+            location.href = "index-lan.php";
+        </script>
+    <?php
     } else {
-        echo "上架成功!";
+        ?>
+        <script>
+            alert("上架成功!");
+            location.href = "index-lan.php";
+        </script>
+    <?php
     }
+    
 } else {
     echo "請選擇房屋設備!";
 }
