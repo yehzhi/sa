965a0a4aed3717a1bc208353009f3f81aebd13be
yo08177 <105029848+yo08177@users.noreply.github.com>
2024-05-13 20:04:14 +0800

    Update verify.php

--- verify.php
+++ verify.php
@@ -19,7 +19,7 @@ $tmp_name2 = $_FILES['photo2']['tmp_name'];
 $target_file2 = $target_dir . basename($photo2);
 move_uploaded_file($tmp_name2, $target_file2);
 
-$sql_max_id = "SELECT MAX(v_id) AS max_id FROM verify";
+$sql_max_id = "SELECT MAX(vid) AS max_id FROM verify";
 $result = $conn->query($sql_max_id);
 $row = $result->fetch_assoc();
 $max_id = $row["max_id"];
@@ -31,16 +31,15 @@ if ($max_id === null) {
 $new_id = $max_id + 1;
 
 
-$sql = "INSERT INTO verify (v_id,v_land, v_prove,v_address) VALUES ('$new_id','$target_file1', '$target_file2','$vaddress')";
+$sql = "INSERT INTO verify (vid,v_land, v_prove,v_address) VALUES ('$new_id','$target_file1', '$target_file2','$vaddress')";
 
 if ($conn->query($sql) !== TRUE) {
     echo "提交驗證失敗!";
 
 } else {
-    echo "提交驗證成功!你的房屋編號是:",$new_id ;
+    echo "提交驗證成功!你的房屋編號是:",$new_id ;   
 }
 
 
 $conn->close();
 ?>
-
