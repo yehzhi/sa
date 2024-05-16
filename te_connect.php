d5da7630521c8ac1458c9064d99d3885694bdfbb
lic85 <liicheng85@gmail.com>
2024-05-16 18:18:43 +0800

    hi

--- te_connect.php
+++ te_connect.php
@@ -1,8 +1,8 @@
 <?php
-    $servername="127.0.0.1";
-    $username="root";
-    $password="";
-    $dbname="dt_m";
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = ""; 
+    $dbname = "dt_m";
 
 $conn=new mysqli($servername,$username,$password,$dbname);
 if($conn->connect_error){
