eda60dc3e7fd0b6039c71e61220ec453348f1284
yo08177 <105029848+yo08177@users.noreply.github.com>
2024-06-08 05:38:55 +0800

    新增詳細資料

--- index-te.php
+++ index-te.php
@@ -541,6 +541,8 @@ session_start();
                     echo "<div class='room_tags'>";
                     echo "<span class='room_tag'>" . $row["i_gender"] . "</span>";
                     echo "<span class='room_tag'>離" . $row["i_entrance"] . " " . $row["i_walktime"] . "</span>";
+		    echo "<br>";
+                    echo "<a href='detail.php?vid=".$row["vid"]."'>詳細資料</a><br>";
 
                     echo "</div>";
                     echo "</div>";
