0222b8c298aba9edf7e240b84d0d1c8ac65f4e5d
yumian0608 <Peggy.2004.0608@gmail.com>
2024-06-10 22:25:33 +0800

    房客改密碼

--- passw.php
+++ passw.php
@@ -58,5 +58,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "無效的用戶類型。";
     }
+    if ($usertype === 't') {
+        $sql = "SELECT password FROM te_member WHERE account = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $account);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($dbPassword);
+        $stmt->fetch();
+
+        if ($current_password === $dbPassword) {
+            // 驗證新密碼和確認新密碼
+            if ($new_password === $confirm_password) {
+                // 更新密碼
+                $updateSql = "UPDATE te_member SET password = ? WHERE account = ?";
+                $updateStmt = $conn->prepare($updateSql);
+                $updateStmt->bind_param("ss", $new_password, $account);
+                if ($updateStmt->execute()) {
+                    echo "<script>alert('密碼更新成功');</script>";
+                    echo"<script>window.location.href='info.php';</script>";
+                    exit();
+                } else {
+                    echo"<script>alert('密碼更新失敗,請重試');</script>"; 
+                    echo"<script>window.location.href='info.php';</script>";
+                }
+            } else {
+                echo "<script>alert('新密碼和確認新密碼不一致。');</script>"; 
+                echo"<script>window.location.href='info.php';</script>";
+            }
+        } else {
+            echo "<script>alert('當前密碼錯誤,可以從忘記密碼重新設定。');</script>"; 
+            echo"<script>window.location.href='info.php';</script>";
+        }
+    } else {
+        echo "無效的用戶類型。";
+    }
 }
 ?>
